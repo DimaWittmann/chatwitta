a7fd62e070fc8753a246b1515feeb045f92f4310
DimaWittmann <dimawittmann@gmail.com>
2014-08-24 13:20:45 +0300

    port

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
 from app import socketio, app
 
-socketio.run(app, host='0.0.0.0')
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
